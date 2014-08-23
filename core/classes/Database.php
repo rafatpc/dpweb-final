924a93a42743d853c95a7df36476e9005001a890
Atanas Harapov <Athov@users.noreply.github.com>
2014-08-24 02:15:32 +0300

    Update Database.php

--- core/classes/Database.php
+++ core/classes/Database.php
@@ -20,7 +20,6 @@ class Database {
     {
         $options = array(
             PDO::ATTR_PERSISTENT => true, 
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         );
         try {
