4618adca689101881e5b8aca8c164588c3ca12cd
Atanas Harapov <Athov@users.noreply.github.com>
2014-08-25 20:41:21 +0300

    Create paths.php

---
+++ mvc/app/config/paths.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    
+    'views' => APP . 'views' . DIRECTORY_SEPARATOR,
+    // 'controllers' => '', this path is edited in the /index.php
+    'models' => APP . 'models' . DIRECTORY_SEPARATOR,
+    
+);
