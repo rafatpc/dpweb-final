35f9154fd635eba1a3da5749551345054536acc1
Atanas Harapov <Athov@users.noreply.github.com>
2014-08-25 18:03:23 +0300

    Update CoreModel.php

--- mvc/core/classes/CoreModel.php
+++ mvc/core/classes/CoreModel.php
@@ -3,40 +3,11 @@ namespace Core\Classes;
 
 class CoreModel
 {
-    protected $use_orm = false;
-    protected $primary_key = null;
-    protected $table = null;
-    protected $db_driver = null; // valid types are MySQL,MSSQL
+    public $db = null;
 
-    private function isOrm()
+    public function __construct($driver = null)
     {
-        if (!$this->use_orm) {
-            throw new \Exception('ORM is not enable!');
-        }
-    }
-    
-    public function db($driver = null)
-    {
-        $this->isOrm();
-
-        if(!empty($driver)){
-            $this->db_driver = $driver;
-        }
-        $db_driver = "\\" . $this->db_driver;
-        return new $db_driver();
-    }
-    
-    public function select($what = '*', $fetch_mode = null)
-    {
-        $this->isOrm();
-        $select = $this->db()->select($what)->from($this->table);
-        
-        if(!empty($fetch_mode)){
-            $build = $select->build(true, $fetch_mode);
-        } else {
-           $build = $select->build(true);
-        }
-        
-        return $build;
+        $driver = '\\' .\Config::get('database.driver');
+        $this->db = new $driver();
     }
 }
