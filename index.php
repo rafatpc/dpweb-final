0a1779e59f4e83601687057ac9f13dd24df260f2
Liliyan Krumov <lilyan@hop-online.com>
2014-08-25 14:44:49 +0300

    Autoloading flexibility added; Smarty included

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
 require_once './application/DPWeb.php';
+require './libs/Smarty/Smarty.class.php';
 
+$smarty = new Smarty;
 
-$app = new DPWeb\Application\DPWeb();
+try {
+    $app = new \DPWeb\Application\DPWeb();
+} catch (Exception $exc) {
+    echo "<pre>";
+    echo $exc->getMessage();
+    echo "</pre>";
+}
