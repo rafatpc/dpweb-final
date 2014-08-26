0a90e93d25ca5f2dbb7f9337220fdd2bdfd29915
Liliyan Krumov <lilyan@hop-online.com>
2014-08-26 12:25:55 +0300

    Simple database driver; Config loader

---
+++ application/Config.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DPWeb\Application;
+
+class Config {
+
+    public $configArray = array();
+    public $configFolder = 'config';
+    public $extension = '.php';
+    public static $instance = null;
+
+    private function __construct() {
+        $this->configArray['main'] = parse_ini_file($this->config['ini_file']);
+    }
+
+    public function loadConfig($filename) {
+        if (isset($this->configArray[$filename])) {
+            return $this->configArray[$filename];
+        }
+
+        $filepath = $this->configFolder . DIRECTORY_SEPARATOR . $filename . $this->extension;
+
+        if (file_exists($filepath) && is_readable($filepath)) {
+            $this->configArray[$filename] = include_once $filepath;
+            return $this->configArray[$filename];
+        } else {
+            throw new \Exception('Couldn\'t load config file.');
+        }
+    }
+
+    public function __get($config) {
+        return $this->loadConfig($config);
+    }
+
+    /**
+     * 
+     * @return Config
+     */
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Config;
+        }
+
+        return self::$instance;
+    }
+}
---
+++ config/Config.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'ini_file' => 'C:\\dpweb.ini'
+);
