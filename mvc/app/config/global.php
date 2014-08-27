7f7eadeb7ea69ffd32e3e63e4ad997bfa5f3b5b9
Liliyan Krumov <lilyan@hop-online.com>
2014-08-27 11:41:30 +0300

    Unused files deleted; MeMoS's MVC framework separated; #TODO# Error Handling

--- mvc/app/config/global.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    
-    // the web site whole url address
-    // example: http://localhost/mvc/framework/ MUST end with trailing slash
-    'url' => 'http://localhost/netb/OOP/',
-);
--- mvc/app/config/paths.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    
-    // controllers path is edited in the /index.php
-    'views' => APP . 'views' . DIRECTORY_SEPARATOR,
-    'models' => APP . 'models' . DIRECTORY_SEPARATOR,
-    
-);
