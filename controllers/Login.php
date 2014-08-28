f693be9bfb56d33d8ba9d32c7c7ffa12365d8261
Atanas Harapov <Athov@users.noreply.github.com>
2014-08-28 15:20:22 +0300

    Update Login.php

--- controllers/Login.php
+++ controllers/Login.php
@@ -6,7 +6,9 @@ class Login {
 
     public function index() {
         if (isset($_POST['login']) && !isset($_SESSION['dpw_user'])) {
-            new \DPWeb\Models\User\Login($_POST['username'], $_POST['password']);
+            // new \DPWeb\Models\User\Login($_POST['username'], $_POST['password']);
+            // see the index.php and Autoloader::setAliases
+            new \Model_Login($_POST['username'], $_POST['password']);
         }
 
         View::getInstance()->render('home');
